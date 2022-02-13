66ffbf0c3a0f4d0cd78d474b99ec87ae5196bd12
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-02-13 14:31:21 +0600

    input pin done

--- js/app.js
+++ js/app.js
@@ -16,5 +16,21 @@ function genaratePin(){
     //getting pin
     const pin = getPin();
     console.log(pin)
-    document.getElementById('generate-pin-input').value = pin;
+    document.getElementById('generate-pin').value = pin;
 }
+
+document.getElementById('button-section').addEventListener('click', function (event){
+    const clicked = event.target.innerText;
+    const inputPin = document.getElementById('input-btn');
+    if(isNaN(clicked)){
+        if(clicked == "C"){
+            inputPin.value = '';
+        }else if(clicked == "<"){
+            inputPin.value = inputPin.value.substr(1,inputPin.value.length-1);
+        }
+
+    }else{
+        inputPin.value = inputPin.value + clicked;
+        console.log(event.target.innerText);
+    }
+});
