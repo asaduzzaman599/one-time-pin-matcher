1e4b7dcd8666b273f8d363e82c676e207e9db8e4
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-02-13 14:56:52 +0600

    done

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 
+let actionnumber = 5;
 
 
 //genarate pin number
 function getPin(){
     const pin = Math.round(Math.random()*10000);
     if(pin < 1000){
-        console.log("less then 1000",pin)
+        // console.log("less then 1000",pin)
 
         return getPin();
     }
@@ -15,13 +16,13 @@ function getPin(){
 function genaratePin(){
     //getting pin
     const pin = getPin();
-    console.log(pin)
+    // console.log(pin)
     document.getElementById('generate-pin').value = pin;
 }
 
 document.getElementById('button-section').addEventListener('click', function (event){
     const clicked = event.target.innerText;
-    const inputPin = document.getElementById('input-btn');
+    const inputPin = document.getElementById('input-pin');
     if(isNaN(clicked)){
         if(clicked == "C"){
             inputPin.value = '';
@@ -31,6 +32,28 @@ document.getElementById('button-section').addEventListener('click', function (ev
 
     }else{
         inputPin.value = inputPin.value + clicked;
-        console.log(event.target.innerText);
+        // console.log(event.target.innerText);
     }
 });
+
+function matchPin(){
+    const genaratedPin = document.getElementById('generate-pin').value;
+    const inputPin = document.getElementById('input-pin').value;
+
+    if(genaratedPin == inputPin){
+        document.getElementById('success-message').style.display = "block";
+        document.getElementById('failure-message').style.display = "none";
+        document.getElementById('action-left').style.display = 'none';
+        actionnumber = 5;
+    }else{
+        document.getElementById('failure-message').style.display = "block";
+        document.getElementById('success-message').style.display = "none";
+        actionnumber--;
+        if(actionnumber < 1){
+            document.getElementById('submit-btn').setAttribute( 'disabled',true);
+        }
+        document.getElementById('action-left-number').innerText = actionnumber;
+        document.getElementById('action-left').style.display = 'block';
+        
+    }
+}
